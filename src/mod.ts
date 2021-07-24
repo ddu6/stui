75440129388fd992318a97959e717111f06bf11c
DDU <ddu6@protonmail.com>
2021-07-25 06:34:03 +0800

    update

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,6 @@
 export * from './common'
 export * from './shell'
-export * from './lrStruct'
+export * from './lr-struct'
 export * from './touch'
 export * from './lib/imgs'
 export * as icons from './lib/icons'
