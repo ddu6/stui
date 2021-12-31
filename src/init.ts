34e8d5f3cc0048b6c0e33113fc4f429f0ff8fe86
DDU <ddu6@protonmail.com>
2021-12-31 23:48:30 +0800

    update

--- src/init.ts
+++ src/init.ts
@@ -6,23 +6,23 @@ export interface InitOptions{
 export function init(options:InitOptions={}){
     const root=options.root??window
     const {document}=root
-    if(root.document.head.querySelector('meta[charset]')===null){
+    if(document.head.querySelector('meta[charset]')===null){
         const meta=document.createElement('meta')
         meta.setAttribute('charset','utf8')
-        root.document.head.append(meta)
+        document.head.append(meta)
     }
-    if(root.document.head.querySelector('meta[name="viewport"]')===null){
+    if(document.head.querySelector('meta[name="viewport"]')===null){
         const meta=document.createElement('meta')
         meta.name='viewport'
         meta.content='width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0'
-        root.document.head.append(meta)
+        document.head.append(meta)
     }
     const style=document.createElement('style')
     style.textContent=all
-    root.document.head.append(style)
+    document.head.append(style)
     if(options.css!==undefined){
         const style=document.createElement('style')
         style.textContent=options.css
-        root.document.head.append(style)
+        document.head.append(style)
     }
 }
