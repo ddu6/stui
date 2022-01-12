e96f63266334eded2299f1cb1740d3ee0aebf3d0
DDU <ddu6@protonmail.com>
2022-01-13 02:37:30 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,6 +1,3 @@
 import {createASStruct, init} from '../mod.js'
 init()
-const style = document.createElement('style')
-style.textContent = `@import url("../main.css");`
-document.head.append(style)
 document.body.append(createASStruct().element)
