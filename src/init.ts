79a52871880fb5246334b70819449bffe56160a0
DDU <ddu6@protonmail.com>
2022-01-16 17:22:15 +0800

    update

--- src/init.ts
+++ src/init.ts
@@ -7,7 +7,7 @@ export function init() {
     if (document.head.querySelector('meta[name="viewport"]') === null) {
         const meta = document.createElement('meta')
         meta.name = 'viewport'
-        meta.content = 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0'
+        meta.content = 'width=device-width, initial-scale=1'
         document.head.append(meta)
     }
 }
